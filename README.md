f51b2d6b418abd5f1f6202e17aac0cd35a7897bd
komarikl <ladyxa@yandex.ru>
2015-06-01 01:23:14 +0400

    second

---
+++ README.md
@@ -0,0 +1,4 @@
+„тобы установить данный проект, необходимо выполнить следующие действи€:
+1. git clone https://github.com/komarikl/HW1.git
+2. npm install
+3. bower install
--- gulpfile.js
+++ gulpfile.js
@@ -1,30 +1,41 @@
-var gulp = require("gulp"),  connect = require("gulp­connect"); 
-   // Запуск сервера 
-  gulp.task('connect', function() {  
-  	connect.server({  
-  		root: 'app',  
-  		livereload: true, 
-  		port: 8888  });
-  });
-  // Работа с html
- gulp.task('html', function () {
-  	gulp.src('./app/*.html')
-  	.pipe(connect.reload());
-  });
-  // Работа с css
-  gulp.task('css', function () {
-  	gulp.src('./app/css/*.css')
-  	.pipe(connect.reload());
-  });
-  // Работа с js
-  gulp.task('js', function () {
-  	gulp.src('./app/js/*.js')
-  	.pipe(connect.reload());
-  });
-  // Слежка
-  gulp.task ('watch', function () {
-  	gulp.watch(['./app/*.html'], ['html']);
-  	gulp.watch(['./app/css/*.css'], ['css']);
-  	gulp.watch(['./app/js/*.js'], ['js']);  });
-  // Задача по­умолчанию
-  gulp.task('default', ['connect', 'watch']);  
+var gulp = require("gulp"),
+	connect = require("gulp-connect"),
+	opn = require("opn");
+
+// Start server
+gulp.task('connect', function() {
+	connect.server({
+	root : 'app',
+	livereload : true,
+	port: 8888
+	});
+	opn('http://localhost:8888');
+});
+
+// Work with html
+gulp.task('html', function() {
+	gulp.src('./app/*.html')
+	.pipe(connect.reload());
+});
+
+// Work with CSS
+gulp.task('css', function() {
+	gulp.src('./app/css/*.css')
+	.pipe(connect.reload());
+});
+
+// Work with js
+gulp.task('js', function() {
+	gulp.src('./app/js/*.js')
+	.pipe(connect.reload());
+});
+
+// Watching
+gulp.task('watch', function() {
+	gulp.watch(['./app/*.html'], ['html']);
+	gulp.watch(['./app/*.css'], ['css']);
+	gulp.watch(['./app/*.js'], ['js']);
+});
+
+// Default task
+gulp.task('default', ['connect', 'watch']);
